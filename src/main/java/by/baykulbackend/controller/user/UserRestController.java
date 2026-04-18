7c4b5340551d06f98e382d88d78378807485e9c3
DzimaSh <dzmitry.shushkevich@gmail.com>
2026-04-18 13:00:00 +0300

    feat(user): enable user registration and add admin approval workflow

--- src/main/java/by/baykulbackend/controller/user/UserRestController.java
+++ src/main/java/by/baykulbackend/controller/user/UserRestController.java
@@ -460,7 +460,7 @@ public class UserRestController {
                     )
             )
     })
-//    @PostMapping("/registration")
+    @PostMapping("/registration")
     public ResponseEntity<?> registration(@RequestBody @JsonView(Views.UserView.Post.class) User user) {
         return userService.registerUser(user);
     }
